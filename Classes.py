eadaf26f542faae873fb6df5249e2963c66b66c4
frabbit01 <francesca.visalli@mail.polimi.it>
2024-06-25 16:44:38 +0000

    Solo una prima bozza

---
+++ Classes.py
@@ -0,0 +1,25 @@
+#####
+#Solo una bozza per la struttura generale della classe e  cosa mi serve per procedere a definire la classe#
+####
+
+import abc
+from abc import ABC
+#import jax
+class importData(ABC):
+    def __init__(self,patients):
+        self.patients_dict=patients #sarà dizionario di pazienti
+    #metodi da aggiungere
+    
+    
+class import_segm(importData):
+    def __init__(self, patients):
+        super().__init__(patients)
+    
+    def read_segm_geom():
+        pass
+
+class import_img(importData):
+    def __init__(self, patients):
+        super().__init__(patients)
+    def read_img():
+        pass
